6e084ea7cc7c2932ea3b280a7ace6ad91be9f7bf
Nahom <zerihunnahom@gmail.com>
2023-05-15 19:09:47 +0300

    Inherit from person class

--- student.rb
+++ student.rb
@@ -1,9 +1,14 @@
+require_relative "./person.rb"
 
-class Student 
-    
+class Student < Person
+    def initialize(classroom,age,name="Unknown",parent_permission:true)
+        super(age, name, parent_permission:true)
+        @classroom = classroom
+    end
+
+    attr_accessor :classroom
+
     def play_hooky
         "¯\(ツ)/¯"
     end
 end
-  
-  
