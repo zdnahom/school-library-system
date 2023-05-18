7c2cacd9a132ad7adcee08bf25eeafd20f9462b8
Nahom <zerihunnahom@gmail.com>
2023-05-18 17:07:05 +0300

    Add list person and add person functionality

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require_relative './book'
 
 class App
     @@books = []
+    @@people = []
     def list_all_books
         @@books.each do |book|
             puts "Title: '#{book.title}' , Author: #{book.author}"
@@ -16,6 +17,23 @@ class App
     def add_book(title,author)
         book = Book.new(title,author)
         @@books.push(book)
+    end
+
+    def list_all_person
+       @@people.each do |person|
+        puts "[#{person[0]}]Name: #{person[1].name}, ID: #{person[1].id}, Age: #{person[1].age}"
+       end
+    end
+
+    def add_person(type,age,name,specialization='Unknown',permission = 'y')
+        if type == '1'
+            permission = permission == 'n' ? false : true
+            student = Student.new(age,name,permission)
+            @@people.push(["Student",student])
+        elsif type == '2'
+            teacher = Teacher.new(specialization,age,name)
+            @@people.push(["Teacher",teacher])
+        end
     end
 
 end 
