9e05bf39b3bd500795a35b874cc78414d9e7b8e4
Nahom <zerihunnahom@gmail.com>
2023-05-23 01:09:18 +0300

    Update main file

--- main.rb
+++ main.rb
@@ -1,43 +1,18 @@
 require_relative 'app'
-
-OPTIONS = "Please choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit
-".freeze
+require_relative 'menu_displayer'
+require_relative 'menu'
 
 def main
   app = App.new
   puts 'Welcome to School Library App'
+  menu_displayer = MenuDisplayer.new
+  menu = Menu.new(app)
+
   loop do
     puts
-    puts OPTIONS
+    menu_displayer.display()
     choice = gets.chomp
-    run_app(app, choice)
-  end
-end
-
-def run_app(app, choice)
-  case choice
-  when '1'
-    app.display_books
-  when '2'
-    app.display_people
-  when '3'
-    app.add_new_person
-  when '4'
-    app.add_new_book
-  when '5'
-    app.rent_book
-  when '6'
-    app.display_rented_book
-  else
-    puts 'The App has been terminated, Bye!!'
-    exit
+    menu.choose_option(choice)
   end
 end
 
