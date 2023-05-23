aadd821140847cd2dd69c11f78de233fd667b891
Nahom <zerihunnahom@gmail.com>
2023-05-23 18:56:05 +0300

    Setup store

---
+++ store/store_data.rb
@@ -0,0 +1,12 @@
+require 'json'
+
+class StoreData
+    def save_data(file_name,data)
+        File.write(file_name,data.to_json)
+    end
+    def load_data(file_name)
+        json_data = File.read(file_name)
+        data= JSON.parse(json_data)
+        data
+    end
+end
