3e784255937672358cee1ed28c14f6b928dcbfd6
Nahom <zerihunnahom@gmail.com>
2023-05-24 09:36:40 +0300

    Update save_data method

--- store/store_data.rb
+++ store/store_data.rb
@@ -2,7 +2,8 @@ require 'json'
 
 class StoreData
   def save_data(file_name, data)
-    File.write(file_name, data.to_json)
+    json_data = JSON.pretty_generate(data)
+    File.write(file_name, json_data)
   end
 
   def load_data(file_name)
